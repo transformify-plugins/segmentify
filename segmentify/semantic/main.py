4e09708bb1253c44b1f180b3e6b2f165558bb373
marshuang80 <marshuang80@gmail.com>
2019-07-25 13:49:07 -0700

    modifed segmentic with different featurizers

--- segmentify/semantic/main.py
+++ segmentify/semantic/main.py
@@ -1,32 +1,98 @@
 import numpy as np
+import torch
+import skimage
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils.validation import check_is_fitted
-from skimage.filters import gaussian
+from segmentify.model import UNet, layers
+from skimage import feature
 
-def featurize(image, multichannel=False):
-    """Featurize pixels in an image.
+def _load_model(pretrained_model):
+    """Load the featurization model
 
     Parameters
     ----------
-	image: np.ndarray
-		Image data to be featurized.
-	multichannel: bool, optional
-		If image data is multichannel.
+        model_path: str
+            Path to the saved model file
+    """
+    # get pretraineod model saved filed path
+    ## TODO better way to store and define file path
+    if pretrained_model == "HPA":
+        model_path = "/home/mars/CZI/segmentify/segmentify/model/saved_model/UNet_hpa_max.pth"
+    elif pretrained_model == "nuclei":
+        model_path = "/home/mars/CZI/segmentify/segmentify/model/saved_model/UNet_nuclei.pth"
+    else:
+        raise ValueError("pretrained model not defined")
+
+    # load in saved model                                                     
+    # TODO allow gpu
+    pth = torch.load(model_path)
+    model_args = pth['model_args']
+    model_state = pth['model_state']
+    model = UNet(**model_args)
+    model.load_state_dict(model_state)
+
+    # remove last layer and activation                                        
+    model.segment = layers.Identity()
+    model.activate = layers.Identity()
+    model.eval()
+
+    return model
+
+
+def unet_featurize(image, pretrained_model="HPA"):
+    """Featurize pixels in an image using pretrained UNet
+
+    Parameters
+    ----------
+        image: numpy.ndarray
+            Image data to be featurized
+        pretrained_model: str (HPA)
+            name of the pretraine model to use for featurization
 
     Returns
-    ----------
-    features: np.ndarray
-        One feature vector per pixel in the image.
+    -------
+        features: np.ndarray
+            One feature vector per pixel in the image
     """
+    # TODO consider multiple images
+    model = _load_model(pretrained_model)
 
-    if not multichannel:
-        features = np.concatenate([[image],
-                                   [gaussian(image, 2)],
-                                   [gaussian(image, 4)],
-                                   [gaussian(image, 6)]], axis=0)
-        features = np.moveaxis(features, 0, -1)
+    image = torch.Tensor(image).float()
 
+    with torch.no_grad():
+        features = model(image)
+
+    features = features.squeeze().numpy()
+    features = np.transpose(features, (1,2,0))
     return features
+
+
+def filter_featurize(image):
+    """Featurize pixels in an image using various image filters
+
+    Parameters
+    ----------
+        image: np.ndarray
+            Image data to be featurized
+
+    Returns
+    -------
+        features: np.ndarray
+            One feature vector per pixel in the image
+    """
+    # TODO consider multiple images
+    image = np.squeeze(image)
+    features = np.concatenate([[image],
+                               [skimage.filters.gaussian(image,2)],
+                               [skimage.filters.gaussian(image,4)],
+                               [skimage.filters.sobel(image)],
+                               [skimage.filters.laplace(image)],
+                               [skimage.filters.gabor(image, 0.1)[0]],
+                               [skimage.filters.gabor(image, 0.6)[1]],
+                               [feature.canny(image, 1,0)]], axis=0)
+    features = np.moveaxis(features, 0, -1)
+    return features
+
 
 
 def fit(image, labels, multichannel=False):
@@ -34,23 +100,29 @@ def fit(image, labels, multichannel=False):
 
     Parameters
     ----------
-	image: np.ndarray
-		Image data to be classified.
+    image: np.ndarray
+        Image data to be classified.
     labels: np.ndarray
         Sparse classification, where 0 pixels are ingored, and other integer
         values correspond to class membership.
-	multichannel: bool, optional
-		If image data is multichannel.
+    multichannel: bool, optional
+        If image data is multichannel.
 
     Returns
     ----------
     classifier: sklearn.ensemble.RandomForestClassifier
         Object that can perform classifications
     """
+    # TODO model not working with coin example 
 
     clf = RandomForestClassifier(n_estimators=10)
 
-    features = featurize(image, multichannel=multichannel)
+    # TODO should this be elsewhere?
+    while len(image.shape) < 4:
+        image = np.expand_dims(image, 0)
+
+    # TODO better way to choose featurizer
+    features = filter_featurize(image)
 
     X = features.reshape([-1, features.shape[-1]])
     y = labels.reshape(-1)
@@ -63,7 +135,7 @@ def fit(image, labels, multichannel=False):
     return clf
 
 
-def predict(classifier, image, multichannel=False):
+def predict(classifier, image):
     """Train a pixel classifier.
 
     Parameters
@@ -71,9 +143,9 @@ def predict(classifier, image, multichannel=False):
     classifier: sklearn.ensemble.RandomForestClassifier
         Object that can perform classifications
     image: np.ndarray
-		Image data to be classified.
-	multichannel: bool, optional
-		If image data is multichannel.
+        Image data to be classified.
+    multichannel: bool, optional
+        If image data is multichannel.
 
     Returns
     ----------
@@ -81,7 +153,13 @@ def predict(classifier, image, multichannel=False):
         Classification, where integer values correspond to class membership.
     """
 
-    features = featurize(image, multichannel=multichannel)
+    # TODO should this be elsewhere?
+    while len(image.shape) < 4:
+        image = np.expand_dims(image, 0)
+
+    # TODO better way to choose featurizer
+    # repeated featurization from fit
+    features = filter_featurize(image)
 
     X = features.reshape([-1, features.shape[-1]])
 
@@ -91,5 +169,4 @@ def predict(classifier, image, multichannel=False):
     except:
         # If classifer has not yet been fit return zeros
         labels = np.zeros(image.shape, dtype=int)
-
     return labels
